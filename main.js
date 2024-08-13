7bf1bf24f3224297aac3c8a08ba6c8522ec5e1a1
rensilva5 <renzoasilva@gmail.com>
2024-08-13 01:01:04 -0400

    Changes in CSS and adding functions to display number on the sliders

--- main.js
+++ main.js
@@ -7,15 +7,33 @@ const totalTipPaid = document.querySelector(".total-tip-paid");
 const totalAmountToPay = document.querySelector(".total-amount-to-pay");
 const totalTipPercentageValue = document.querySelector(".tip-value");
 const discountPercentageValue = document.querySelector(".discount-value");
+const eachCustomerToPay = document.querySelector(".each-customer-to-pay");
 const totalNoOfCustomersValue = document.querySelector(
   ".no-of-customers-value"
 );
+const updateDiscountValue = document.querySelector(".discount-value");
+
+// Function to update discount value adjusting the sliders
+discountPercentage.addEventListener("input", () => {
+  discountPercentageValue.textContent = discountPercentage.value + "%"
+});
+
+// Function to update tips value when adjusting the sliders
+tipsPercentage.addEventListener("input", () => {
+  totalTipPercentageValue.textContent = tipsPercentage.value + "%"
+});
+
+// Function to update number of customers adjusting the sliders
+noOfCustomers.addEventListener("input", () => {
+  totalNoOfCustomersValue.textContent = noOfCustomers.value;
+});
 
 function calculateBill() {
   const bill = parseFloat(billAmount.value);
   const discount = parseFloat(discountPercentage.value);
   const tips = parseFloat(tipsPercentage.value);
   const customers = parseFloat(noOfCustomers.value);
+  // const AmountPerCustomer = parseFloat(eachCustomerToPay.value);
 
   const billAmountAfterDiscountPercentage = bill - (discount * bill) / 100;
 
@@ -23,11 +41,15 @@ function calculateBill() {
 
   const totalBill = bill + getTipAmount;
 
+  const eachCustomerToPayAmount = totalBill / customers;
+
   totalTipPaid.textContent = getTipAmount;
   totalAmountToPay.textContent = totalBill;
   totalTipPercentageValue.textContent = tipsPercentage.value;
   discountPercentageValue.textContent = discountPercentage.value;
   totalNoOfCustomersValue.textContent = noOfCustomers.value;
+  eachCustomerToPay.textContent = eachCustomerToPayAmount;
+  // updateDiscountValue = document.querySelector(".discount-value");
 }
 
 generateBillButtom.addEventListener("click", calculateBill);
